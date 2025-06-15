0198ee5454f713ef8f3a1998b1b05a74ec6287b6
axdlee <xdlee110@gmail.com>
2025-06-15 23:35:41 +0800

    feat: 添加 Tailwind CSS 支持，更新样式和依赖项，优化 Tauri 配置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,10 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+use tauri::Builder;
+
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+    Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
